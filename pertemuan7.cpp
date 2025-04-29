d434f6e9f60be60dcb20e0118f53df4c34788a8b
galang <ahmad.galang.pamungkas24@gmail.com>
2025-04-29 15:54:02 +0700

    projek 4

--- pertemuan7.cpp
+++ pertemuan7.cpp
@@ -4,15 +4,25 @@ using namespace std;
 class Mahasiswa{
 public  :
   int nim;
-  string nama;
+  string nama;q
   float nilai;
 
   void printData(){
     cout << "Nim : " << nim << endl;
     cout << "Nama: " << nama << endl;
     cout << "Nilai: " << nilai << endl;
+
     
   }
+};
+
+int main() {
+    Mahasiswa mhs;
+    mhs.nim = 2024;
+    mhs.nama = "galang";
+    mhs.nilai = 90.5;
+
+    mhs.printData();
 }
 
 
